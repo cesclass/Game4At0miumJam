52f8af3e4162cc918166e65e449652b45abbc3ff
dcaron1 <dylan.caron81@laposte.net>
2018-07-21 19:42:51 +0200

    presk' oskour

--- scripts/Elder.js
+++ scripts/Elder.js
@@ -3,13 +3,12 @@
 function Elder() {
 
     /*  Attributs  */
-    this.img = window.mainDiv.createElement('img');
+    this.img = document.createElement('img');
 
     /*  Methodes  */
     this.show = (self) => {
         self.img.src = "./img/vieux_120px.png";
         window.mainDiv.appendChild(this.img);
-        self.img.style.position = 'absolute';
         self.img.style.bottom = '40px';
     }
 
